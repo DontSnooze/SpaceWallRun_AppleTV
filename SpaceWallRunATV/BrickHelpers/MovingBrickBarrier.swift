9142bc625603bedec5ea03556003328678f38f33
Amos Todman <>
2019-03-03 23:49:07 -0500

    Wall scene working good

---
+++ SpaceWallRunATV/BrickHelpers/MovingBrickBarrier.swift
@@ -0,0 +1,13 @@
+//
+//  MovingBrickBarrier.swift
+//  SpaceWallRunATV
+//
+//  Created by Amos Todman on 3/3/19.
+//  Copyright © 2019 Amos Todman. All rights reserved.
+//
+
+import UIKit
+
+class MovingBrickBarrier: NSObject {
+
+}
